ec5e1a27520c933313f40e9606031aa3234f6410
Sahi Mohamed Francis Gonsangbeu <mohamedfrancissahi@gmail.com>
2023-11-23 17:30:34 +0100

    refactor: updated test

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -1,13 +1,11 @@
 from fastapi.testclient import TestClient
 
-from app.main import app
+from api.main import app
 
 client = TestClient(app)
 
 
 def test_predict():
     filepath = "tests/files/picture.jpeg"
-    response = client.post(
-        "/predict", files={"file": ("filename", open(filepath, "rb"), "image/jpeg")}
-    )
+    response = client.post("/predict", files={"file": ("filename", open(filepath, "rb"), "image/jpeg")})
     assert response.status_code == 200
