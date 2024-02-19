8e641b68ae5f4578da4b25b032067c9d4b40e96d
Sahi Mohamed Francis Gonsangbeu <mohamedfrancissahi@gmail.com>
2024-02-19 21:04:21 +0100

    fix: fixed file path

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -6,6 +6,6 @@ client = TestClient(app)
 
 
 def test_predict():
-    filepath = "files/picture.jpeg"
+    filepath = "../tests/files/picture.jpeg"
     response = client.post("/predict", files={"file": ("filename", open(filepath, "rb"), "image/jpeg")})
     assert response.status_code == 200
